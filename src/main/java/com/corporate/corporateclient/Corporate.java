c07540dd52212c2ddc1dbea08bb99c29578165ad
Rusty8 <77588005+Rusty8@users.noreply.github.com>
2021-11-14 15:19:42 -0500

    The beginning of an era

---
+++ src/main/java/com/corporate/corporateclient/Corporate.java
@@ -0,0 +1,13 @@
+package com.corporate.corporateclient;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Corporate.modid, name = Corporate.name, version = Corporate.version)
+public class Corporate {
+
+    public static final String modid = "corporate";
+    public static final String version = "v1.0";
+    public static final String name = "Corporate 1.12.2";
+
+}
